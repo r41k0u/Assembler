1a06d79fb8b3f689a15dc8581ca129f2efa5a371
Pragyansh Chaturvedi <pragyanshchaturvedi18@gmail.com>
2022-04-04 18:58:54 +0530

    SIC done

--- assembler.py
+++ assembler.py
@@ -33,6 +33,8 @@ isa_tab = {
 
 sym_tab = {}
 
+out_code = []
+
 for i in range(len(input_stream)):
     input_stream[i] = input_stream[i].split()
 
@@ -43,6 +45,7 @@ for i in range(1, len(input_stream)):
     input_stream[i].append(int(start_addr, 16) + 3*(i-1))
 
     if "RSUB" in input_stream[i]:
+        end_addr = hex(input_stream[i][-1])
         start_addr = hex(0x10000 - i*3)
     elif "RESW" in input_stream[i]:
         start_addr = hex(int(start_addr, 16) - 3 + 3*int(input_stream[i][-2]))
@@ -54,3 +57,59 @@ for i in range(1, len(input_stream)):
 for i in range(len(input_stream)):
     if input_stream[i][0] not in isa_tab:
         sym_tab[input_stream[i][0]] = hex(int(input_stream[i][-1]))
+
+prog_len = str(hex(int(end_addr, 16) - int(input_stream[0][-1], 16)))
+
+header = "H" + input_stream[0][0] + (" "*(5-len(input_stream[0][0]))) + "0"*(6 - len(input_stream[0][-1])) + str(hex(int(input_stream[0][-1], 16)))[2:] + "0"*(8-len(prog_len)) + prog_len[2:]
+
+out_code.append(header)
+
+out_str = "T"
+out_str += "0"*(8-len(str(ref_addr))) + str(ref_addr)[2:]
+
+for i in range(len(input_stream)):
+    if(int(input_stream[i][-1]) - int(ref_addr, 16)) > 9 :
+        out_code.append(out_str)
+        out_str = "T"
+        ref_addr = hex(input_stream[i][-1])
+        out_str += "0"*(8-len(str(ref_addr))) + str(ref_addr)[2:]
+    if input_stream[i][0] == "START" or input_stream[i][1] == "START":
+        pass
+    if input_stream[i][0] in isa_tab:
+        out_str+= "0"*(2 - len(str(hex(isa_tab[input_stream[i][0]]))[2:])) + str(hex(isa_tab[input_stream[i][0]]))[2:]
+        if input_stream[i][0] == "RSUB":
+                out_str+= "0000"
+        if "X" in input_stream[i]:
+            x = 32768
+        else:
+            x = 0
+        if input_stream[i][1] in sym_tab:
+            out_str += "0"*(8 - len(sym_tab[input_stream[i][1]])) + str(hex(int(sym_tab[input_stream[i][1]][2:], 16) + x))[2:]
+        elif str(input_stream[i][1])[0] == '#':
+            out_str += "0"*(8 - len(str(hex(int(input_stream[i][1][1:]))))) + str(hex(int(input_stream[i][1][1:])))[2:]
+    elif input_stream[i][0] in sym_tab:
+        if input_stream[i][1] in isa_tab:
+            out_str+= "0"*(2 - len(str(hex(isa_tab[input_stream[i][1]]))[2:])) + str(hex(isa_tab[input_stream[i][1]]))[2:]
+            if input_stream[i][1] == "RSUB":
+                out_str+= "0000"
+            if "X" in input_stream[i]:
+                x = 32768
+            else:
+                x = 0
+            if input_stream[i][2] in sym_tab:
+                out_str += "0"*(8 - len(sym_tab[input_stream[i][2]])) + str(hex(int(sym_tab[input_stream[i][2]][2:], 16) + x))[2:]
+            elif str(input_stream[i][1])[0] == '#':
+                out_str += "0"*(8 - len(str(hex(int(input_stream[i][2][1:]))))) + str(hex(int(input_stream[i][2][1:])))[2:]
+        elif input_stream[i][1] == "WORD":
+            out_str += "0"*(10 - len(str(hex(int(input_stream[i][2]))))) + str(hex(int(input_stream[i][2])))[2:]
+
+out_code.append(out_str)
+            
+out_code.append("E" + "0"*(6 - len(input_stream[0][-1])) + str(hex(int(input_stream[0][-1], 16)))[2:])
+
+for i in range(len(out_code)):
+    out_code[i] += "\n"
+
+out = open("asm.obj", "w")
+out.writelines(out_code)
+out.close()
